3e7d8ff5e4220edd7778bbe5e11e62ed1ab57054
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-03-04 18:45:11 +0530

    new commit

--- React_JS/src/API/Axios/Get.jsx
+++ React_JS/src/API/Axios/Get.jsx
@@ -1,9 +1,11 @@
+import axios from 'axios'
 
 export default function Get(){
     async function getting(){
         try{
+            const response = await axios.get('http://localhost:3000/users')
         } catch(error){
-
+            console.log(error.message);
         }
     }
     return(
