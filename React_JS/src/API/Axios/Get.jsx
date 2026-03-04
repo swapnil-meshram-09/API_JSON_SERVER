fb8abced3453236023bec844562500bd8d616027
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-03-04 18:58:13 +0530

    new commit

--- React_JS/src/API/Axios/Get.jsx
+++ React_JS/src/API/Axios/Get.jsx
@@ -1,19 +1,29 @@
 import axios from 'axios'
+import { useState, useEffect } from 'react';
 
 export default function Get(){
+    const [value, setValue] = useState([])
+
+    useEffect(()=>{
     async function getting(){
         try{
             const response = await axios.get('http://localhost:3000/users')
             const data = response.data
             console.log(data);
-            
+            setValue(data)
         } catch(error){
             console.log(error.message);
         }
     }
+    getting()
+    },[])
     return(
         <>
-        
+        {
+            value.map((val, idx)=>(
+                <h1 key={idx}>{val.name}</h1>
+            ))
+        }
         
         </>
     )
