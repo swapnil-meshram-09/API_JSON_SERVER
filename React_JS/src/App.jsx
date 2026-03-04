976af852d6f3b64c4f114eef03a0f1760d0f79a1
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-03-04 18:59:33 +0530

    new commit

--- React_JS/src/App.jsx
+++ React_JS/src/App.jsx
@@ -1,9 +1,11 @@
 import Get from './API/Axios/Get'
+import Post from './API/Axios/Post'
 
 export default function App(){
   return(
     <>
-    <Get />
+    {/* <Get /> */}
+    <Post />
     </>
   )
 }
