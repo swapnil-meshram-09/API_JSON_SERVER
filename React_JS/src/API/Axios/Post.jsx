70875c98e78857b18872e8b59034e90341781bff
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-03-04 19:00:47 +0530

    new commit

--- React_JS/src/API/Axios/Post.jsx
+++ React_JS/src/API/Axios/Post.jsx
@@ -1,4 +1,13 @@
+import axios from 'axios'
+
 export default function Post(){
+    async function Posting(){
+        try{
+
+        } catch(error){
+            console.log(error.message); 
+        }
+    }
     return(
         <>
         
