7209e43bb620cb36674888fec6628bf6f27efb8d
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-03-05 18:51:10 +0530

    new commit

--- React_JS/src/API/Axios/Post.jsx
+++ React_JS/src/API/Axios/Post.jsx
@@ -2,6 +2,10 @@ import axios from 'axios'
 import { useState, useEffect } from 'react';
 
 export default function Post(){
+    const [id, setId] = useState('')
+    const [name, setName] = useState('')
+    const [age, setAge] = useState('')
+
     async function Posting(){
         try{
 
