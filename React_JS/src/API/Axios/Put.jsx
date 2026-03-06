f4a874a5875083b25e6aeae92d9bc6ad3eba8f08
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-03-06 18:38:09 +0530

    new commit

--- React_JS/src/API/Axios/Put.jsx
+++ React_JS/src/API/Axios/Put.jsx
@@ -1,10 +1,17 @@
 import axios from 'axios'
-import { useState } from 'axios'
+import { useState } from 'react'
 
 export default function Put(){
+    const [name, setName] = useState('')
+    const [age, setAge] = useState('')
+
     return(
         <>
-        
+        <form action="">
+            <input type="text" placeholder='enter name' value={name} onChange={(e)=>setName(e.target.value)}/><br /><br />
+            <input type="text" placeholder='enter age' value={age} onChange={(e)=>setAge(e.target.value)}/><br /><br />
+            <button>Click</button>
+        </form>
         </>
     )
 }
