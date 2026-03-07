60a69f4e06185f57e68e2e3883a23170924202f0
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-03-07 18:45:54 +0530

    new commit

--- React_JS/src/API/Axios/Patch.jsx
+++ React_JS/src/API/Axios/Patch.jsx
@@ -8,6 +8,16 @@ export default function Patch(){
         try{
             const response = await axios.get(`http://localhost:3000/users?name=${}`)
 
+            if(response.data.length === 0){
+                console.log('User not found');
+            }
+
+            const userId = response.data[0].userId
+
+            const dataUpdate = await axios.patch(`http://localhost:3000/users/${userId}`,
+                { age }
+            )
+
         } catch(error){
             console.log(error.message);
         }
