36e3c2868e1a06451f8e057da5bcb1e5a829c512
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-03-07 18:41:36 +0530

    new commit

--- React_JS/src/API/Axios/Patch.jsx
+++ React_JS/src/API/Axios/Patch.jsx
@@ -4,10 +4,16 @@ import { useState } from 'react'
 export default function Patch(){
     const posting = async(e)=>{
         e.preventDefault()
+
+        try{
+            
+        } catch(error){
+            console.log(error.message);
+        }
     }
     return(
         <>
-        <form action="" onSubmit={}>
+        <form action="" onSubmit={posting}>
             <input type="text" /><br /><br />
             <input type="text" /><br /><br />
             <button>Click</button>
