05c0c1201316006431e59c1ae926f415bd4e6526
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-03-07 18:38:02 +0530

    new commit

--- React_JS/src/API/Axios/Patch.jsx
+++ React_JS/src/API/Axios/Patch.jsx
@@ -4,7 +4,11 @@ import { useState } from 'react'
 export default function Patch(){
     return(
         <>
-        
+        <form action="">
+            <input type="text" /><br /><br />
+            <input type="text" /><br /><br />
+            <button>Click</button>
+        </form>
         </>
     )
 }
