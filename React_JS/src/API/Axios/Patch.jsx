c3592bd0cd6f1cd40e5d058457f36198454e61a0
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-03-07 18:48:15 +0530

    new commit

--- React_JS/src/API/Axios/Patch.jsx
+++ React_JS/src/API/Axios/Patch.jsx
@@ -1,7 +1,10 @@
 import axios from 'axios'
-import { useState } from 'react'
+import { use, useState } from 'react'
 
 export default function Patch(){
+    const [name, setName] = useState('')
+    const [age, setAge] = useState('')
+
     const pathcing = async(e)=>{
         e.preventDefault()
 
@@ -17,7 +20,6 @@ export default function Patch(){
             const dataUpdate = await axios.patch(`http://localhost:3000/users/${userId}`,
                 { age }
             )
-
         } catch(error){
             console.log(error.message);
         }
@@ -25,8 +27,8 @@ export default function Patch(){
     return(
         <>
         <form action="" onSubmit={patching}>
-            <input type="text" placeholder='enter name' value={} onChange={}/><br /><br />
-            <input type="text" placeholder='enter age' value={} onChange={}/><br /><br />
+            <input type="text" placeholder='enter name' value={name} onChange={(e)=>setName(e.target.value)}/><br /><br />
+            <input type="text" placeholder='enter age' value={age} onChange={(e)=>setAge(e.target.value)}/><br /><br />
             <button>Click</button>
         </form>
         </>
