eb74e51c1158bd979f98f18383b51ffe7b46c762
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-03-08 18:47:53 +0530

    new commit

--- React_JS/src/API/Axios/Delete.jsx
+++ React_JS/src/API/Axios/Delete.jsx
@@ -2,10 +2,16 @@ import axios from 'axios'
 import { useState } from 'useState'
 
 export default function Delete(){
+    const [name, setName] = useState('')
+
+    const deleting = async(e) =>{
+        e.preventDefault()
+
+    }
     return(
         <>
-        <form action="">
-            <input type="text" placeholder='enter name' value={} onChange={(e)=>e.target.value}/><br /><br />
+        <form action="" onSubmit={deleting}>
+            <input type="text" placeholder='enter name' value={name} onChange={(e)=>setName(e.target.value)}/><br /><br />
             <button>Click</button>
         </form>
         </>
