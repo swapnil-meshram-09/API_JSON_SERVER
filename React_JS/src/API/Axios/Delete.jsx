51fbe4a5c30b7f4b25f41941f71fd01e75c4ab85
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-03-08 18:45:18 +0530

    new commit

--- React_JS/src/API/Axios/Delete.jsx
+++ React_JS/src/API/Axios/Delete.jsx
@@ -4,7 +4,11 @@ import { useState } from 'useState'
 export default function Delete(){
     return(
         <>
-        
+        <form action="">
+            <input type="text" /><br /><br />
+            <input type="text" /><br /><br />
+            <button>Click</button>
+        </form>
         </>
     )
 }
