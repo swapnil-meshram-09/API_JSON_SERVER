f19978cefda499e67edde3c4c73b6a4788b2cd5f
Swapnil Meshram <swapnilmeshram514@gmail.com>
2026-03-08 18:43:50 +0530

    new commit

--- React_JS/src/App.jsx
+++ React_JS/src/App.jsx
@@ -3,6 +3,7 @@ import Get from './API/Axios/Get'
 import Post from './API/Axios/Post'
 import Put from './API/Axios/Put'
 import Patch from './API/Axios/Patch'
+import Delete from './API/Axios/Delete'
 
 export default function App(){
   return(
@@ -10,8 +11,8 @@ export default function App(){
     {/* <Get /> */}
     {/* <Post /> */}
     {/* <Put /> */}
-    <Patch />
-
+    {/* <Patch /> */}
+    <Delete />
     </>
   )
 }
